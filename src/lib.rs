8bc428025ccdd63e0282342cb22b6d25295ae656
mc_fdc <Masato.04.11.2007@gmail.com>
2023-07-03 12:46:26 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
 use pyo3::prelude::*;
+use std:fs;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
+
+#[pyclass]
+struct File {
+    file: fs::File,
+}
+
+#[pymethods]
+impl File {
+    #[new]
+    fn open(path: &str) -> Self {
+        Self {
+            file: fs::File::opem(path).unwrap()
+        }
+    }
+}
+
 
 /// A Python module implemented in Rust.
 #[pymodule]
